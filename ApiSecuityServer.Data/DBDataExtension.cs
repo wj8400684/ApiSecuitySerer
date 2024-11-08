ddd0312759f9b648e05206166e307f70670a59ca
wujun <8400684@qq.com>
2024-11-09 00:38:30 +0800

    ini

--- ApiSecuityServer.Data/DBDataExtension.cs
+++ ApiSecuityServer.Data/DBDataExtension.cs
@@ -1,3 +1,4 @@
+using EntityFrameworkCore.UnitOfWork.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,8 @@ public static class DBDataExtension
         services.AddDbContextPool<ApplicationDbContext>(optionsAction: options =>
         {
             options.UseSqlite(connectionString);
-        });
+        }).AddScoped<DbContext, ApplicationDbContext>()
+        .AddUnitOfWork()
+        .AddUnitOfWork<ApplicationDbContext>();
     }
 }
